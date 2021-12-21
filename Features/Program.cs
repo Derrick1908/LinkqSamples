1e42c82510dff43fcde2d670d95e43487b3a6514
Derrick Nazareth <derrick.nazareth@teknorix.com>
2021-12-21 10:55:10 +0530

    Used Func & Action Types; Named Method, Anonymous Method and Lambda Expressions to Filter Employees;

--- Features/Program.cs
+++ Features/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using Features.Linq;
+using System.Linq;
+//using Features.Linq;
 
 namespace Features
 {
@@ -8,10 +9,23 @@ namespace Features
     {
         static void Main(string[] args)
         {
+            Func<int, int> square = x => x * x;                 //Last argument in the List is the return type. It can receive upto 16 Arguments.
+            Func<int, int, int> add = (x, y) => x + y;
+
+            Func<int, int, int> add2 = (x, y) =>            //Can also include a Body for the Function, but note that when we include curly Braces i.e.e Body we have
+            {                                               //Include the return Statement.    
+                int temp = x + y;
+                return temp;
+            };
+            Action<int> write = x => Console.WriteLine(x);
+
+            write(square(add(5, 3)));
+
             Employee[] developers = new Employee[]
             {
                 new Employee{ Id = 1, Name = "Scott" },
-                new Employee{ Id = 2, Name = "Chris" }
+                new Employee{ Id = 2, Name = "Chris" },
+                new Employee{ Id = 8, Name = "Nesser" },
             };
 
             IEnumerable<Employee> advertising = new Employee[]
@@ -27,7 +41,8 @@ namespace Features
 
             IEnumerable<Employee> management = new List<Employee>()
             {
-                new Employee{ Id = 6, Name = "Stark"}
+                new Employee{ Id = 6, Name = "Stark"},
+                new Employee{ Id = 7, Name = "Ben"}
             };
 
             Console.WriteLine($"Number of Employees in Developers Dept : {developers.Count()}");        //Using the Extension Method that we have defined. If we use Linq directive also, it will give an error becoz of Ambiguous Call.
@@ -55,7 +70,45 @@ namespace Features
             {
                 Console.WriteLine("Id " + enumerator.Current.Id + ": " + enumerator.Current.Name);
             }
+
+            //This is used to display Names that start only with S -> Named Method in the Where of LINQ
+            Console.WriteLine("Names Starting with S in Developer Dept::");
+            foreach (var employee in developers.Where(NameStartsWithS))
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            //This is used to display Names that start only with R -> Anonymous Method in the Where of LINQ
+            Console.WriteLine("Names Starting with R in Advertising Dept::");
+            foreach (var employee in advertising.Where(
+                delegate(Employee employee)
+                { return employee.Name.StartsWith("R"); }
+                ))
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+
+            //This is used to display Names that start only with S -> Lambda Expression in the Where of LINQ
+            Console.WriteLine("Names Starting with S in Management Dept::");
+            foreach (var employee in management.Where(e => e.Name.StartsWith("S")))                
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            Console.WriteLine("Sorting of All Employees in the Developer Dept (Name Length is 5 Letters only) by Ascending Order ");
+            foreach (var employee in developers.Where(e => e.Name.Length == 5)
+                                               .OrderBy(e => e.Name))
+            {
+                Console.WriteLine(employee.Name);
+            }
+
             Console.ReadKey();
+        }
+
+        private static bool NameStartsWithS(Employee employee)
+        {
+            return employee.Name.StartsWith("S");
         }
     }
 }
